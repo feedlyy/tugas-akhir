ef4353606906722a1bdcb6e794ca928fcf86694f
feedlyy <muhammadnurfadlifadli@gmail.com>
2018-04-09 15:59:18 +0700

    debugging

--- app/Http/Controllers/ImportExcelController.php
+++ app/Http/Controllers/ImportExcelController.php
@@ -22,23 +22,28 @@ class ImportExcelController extends Controller
                     $result = $reader->toObject();
                     if (!empty($result) && $result->count()){
                         foreach ($result as $key => $value){
-                            $cek = Staff::query()
+                            try{$cek = Staff::query()
                                 ->where('nip', '=', $value->nip)
                                 ->where('nama_staff', '=', $value->nama_staff)
                                 ->get();
-                            $hitung = count($cek);
-                            if ($hitung == 0) {
-                                $staff = new Staff;
-                                $staff->id_fakultas = $value->id_fakultas;
-                                $staff->id_departemen = $value->id_departemen;
-                                $staff->id_prodi = $value->id_prodi;
-                                $staff->nip = $value->nip;
-                                $staff->nama_staff = $value->nama_staff;
-                                $staff->email = $value->email;
-                                $staff->alamat = $value->alamat;
-                                $staff->no_hp = $value->no_hp;
-                                $staff->save();
+                                $hitung = count($cek);
+                                if ($hitung == 0) {
+                                    $staff = new Staff;
+                                    $staff->id_fakultas = $value->id_fakultas;
+                                    $staff->id_departemen = $value->id_departemen;
+                                    $staff->id_prodi = $value->id_prodi;
+                                    $staff->nip = $value->nip;
+                                    $staff->nama_staff = $value->nama_staff;
+                                    $staff->email = $value->email;
+                                    $staff->alamat = $value->alamat;
+                                    $staff->no_hp = $value->no_hp;
+                                    $staff->save();
+                                }}
+                            catch (\Exception $e)
+                            {
+
                             }
+
                         }
                     }
                 })->get();
