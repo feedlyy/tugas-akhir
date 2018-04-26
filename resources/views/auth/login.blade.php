c508d05592f1e250750d698986eb8f0af262c77c
feedlyy <muhammadnurfadlifadli@gmail.com>
2018-04-26 14:53:49 +0700

    update fungsi lupa password seko diana

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,15 +39,6 @@
                             </div>
                         </div>
 
-                        {{--<div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>--}}
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
@@ -55,13 +46,37 @@
                                     Login
                                 </button>
 
-                                {{--<a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>--}}
+
+                                <a data-toggle="modal" data-target="#modal-default" class="fa fa-plus btn btn-danger">
+                                    Lupa Password
+                                </a>
                             </div>
                         </div>
                     </form>
                 </div>
+
+                <div class="modal fade" id="modal-default">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title">Lupa Password</h4>
+                            </div>
+                            <div class="modal-body">
+                                <center>
+                                    <h4>Silahkan hubungi admin fakultas atau departemen terkait untuk perubahan password.</h4>
+                                </center>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                        <!-- /.modal-content -->
+                    </div>
+                    <!-- /.modal-dialog -->
+                </div>
+
             </div>
         </div>
     </div>
